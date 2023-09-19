96ae66978a95ef4b7eae4a102346238ec74390fd
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-09-19 10:16:38 +0530

    image generation connected to the backend

--- src/Components/movieCard.jsx
+++ src/Components/movieCard.jsx
@@ -10,16 +10,17 @@ export function MovieCard(props) {
                 <img src={props.image} alt='movie-image' className='mc-image'></img>
 
                 <div className='mc-content'>
+                    <h1 className='mc-title'>{props.name}</h1>
                     <div className='mc-2'>
-                        <div className='mc-district'>Dimention: {props.dimension}</div>
+                        <div className='mc-district'>Dimension: {props.dimension}</div>
                         <div className='mc-city'>Status: {props.status}</div>
                     </div>
                     <div className='mc-3'>
                         <div className='mc-location'>Genre: {props.genre}</div>
-                        <div className='mc-ratings'>Ratings: {props.ratings}</div>
+                        <div className='mc-ratings'>Lang: {props.language}</div>
                     </div>
                     <div className='mc-4'>
-                        <button className="mc-btn" onClick={props.onClick}>{props.label}</button>
+                        <button className="mc-btn" onClick={() => props.onClick(props.id)}>{props.label}</button>
                     </div>
                 </div>
             </div>
