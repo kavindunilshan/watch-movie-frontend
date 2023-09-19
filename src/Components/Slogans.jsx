e5dee5f207d06c6420615cad0569a9643bee5dcb
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-09-19 10:18:12 +0530

    Add more fluid designs to the components and slogan component is created

---
+++ src/Components/Slogans.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { useInView } from 'react-intersection-observer';
+import "../Styles/slogan.css"
+
+const SloganText = ({text}) => {
+    const [slogan, isStart] = useInView({
+        
+    });
+    return (
+        <div ref={slogan} className={`slogan-container ${isStart ? "annimate-slogan" : ""}`}>
+            <p className='slogan-text'>{text}</p>
+        </div>
+    );
+}
+ 
+export default SloganText;
