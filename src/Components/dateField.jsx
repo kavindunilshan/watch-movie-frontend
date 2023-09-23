d42e16b04aedb1b76b9aadf78e3ba966d8805e8e
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-09-23 22:30:05 +0530

    Resuable components created

---
+++ src/Components/dateField.jsx
@@ -0,0 +1,29 @@
+import { TextField} from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import React, { Children, Component, useState } from 'react'
+
+
+const DateField = () => {
+    const [selectedDate, setSelectedDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    }
+
+    return (
+        <div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker
+                        label="Select Date"
+                        value={selectedDate}
+                        onChange={handleDateChange}
+                        renderInput={(params) => <TextField {...params} 
+                        />}
+                    />
+            </LocalizationProvider>
+        </div>
+    );
+}
+ 
+export default DateField;
---
+++ src/Components/materialDropDown.jsx
@@ -0,0 +1,32 @@
+import React, { Component, useState } from 'react'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function BasicSelect({value, items, label, onChange}) {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  }
+  return (
+    <Box sx={{ minWidth: 120}}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value}
+          label="Age"
+          onChange={(event, key) => onChange(event, key)}
+          sx={{fontSize: "12px"}}
+        >
+          {Array.isArray(items) ? items.map((item, index) => {
+                return <MenuItem key={index} name={index} value={item.id.timeSlot}>{item.id.timeSlot}</MenuItem>;
+          }): ""}
+          
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
