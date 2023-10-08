44483141f4270e88cc696671960ae7674d939530
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-10-08 18:00:58 +0530

    Create custom hook useLogger

---
+++ src/Components/useLogger.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function useLogger(message) {
+    useEffect(() => {
+        if (message !== null) {
+            console.log("Here", message);
+        }
+    }, [])
+}
