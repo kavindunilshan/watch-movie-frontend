a2e956eeda6db672ad184f768f98cc4a83fda0ff
kavindunilshan <kavindunilshanliyanage@gmail.com>
2024-12-13 00:30:41 +0530

    Environment variables set.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from "@asgardeo/auth-react";
 
+const clientID = process.env.ASGARDIO_CLIENT_ID;
+const baseUrl = process.env.ASGARDIO_BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
@@ -14,8 +17,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             config={ {
                 signInRedirectURL: "http://localhost:3000",
                 signOutRedirectURL: "http://localhost:3000",
-                clientID: "7sZC7Fcd92FffmUW5lA6cXwwTA4a",
-                baseUrl: "https://api.asgardeo.io/t/spendwise",
+                clientID: clientID,
+                baseUrl: baseUrl,
                 scope: [ "openid", "profile", "email", "read_movies"]
             } }
         >
